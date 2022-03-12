9e779cb28f1f8f35afe39b43b6fde00ed58e9d4e
Robert McCurdy <rwmccurdy@hotmail.com>
2022-03-12 13:44:27 -0700

    Landing page content

--- reactapp/src/components/LandingPage.js
+++ reactapp/src/components/LandingPage.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import Bench from '../images/bench.png';
+import Curl from '../images/curl.png';
+import Run from '../images/run.png';
+import Rope from '../images/rope.png';
 
 const LandingPage = () => {
-    return <div>LandingPage</div>;
+    const images = [Bench, Curl, Rope, Run];
+    return (
+        <div className='landing-page-container'>
+            <div>
+                <div className='landing-page-header'>Workout and start sharing daily!</div>
+                <div className='landing-page-subheader'>Get daily workout goals and compare your results against others.</div>
+                <button className='landing-page-button'>START SHARING NOW</button>
+            </div>
+            <img className='landing-page-image fade-in' src={images[0]} alt='workout' />
+        </div>
+    );
 };
 
 export default LandingPage;
